ca06e29487882861f2c904ab0b5a51f6e538af59
Yuri Dimitrenko <d89056578550@gmail.com>
2023-02-13 23:46:29 +0300

    Сhanging int to long

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        SalesManager salesManager = new SalesManager(new int[]{5, 16, 7, 28, 0, 55});
+        SalesManager salesManager = new SalesManager(new long []{5, 16, 7, 28, 0, 55});
 
         System.out.println("Максимальное значение = " + salesManager.max());
     }
